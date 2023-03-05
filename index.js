ebb7392087fe654d7dcc32b5d15c0967eeba0c22
shivam singh <126107873+Shivamsin517@users.noreply.github.com>
2023-03-05 12:10:59 +0000

    docs:check dob leap year

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-//check dob leap year?
+console.log('check dob leap year?')
 console.log('check whether your dob is leap year?')
 var readlineSync = require('readline-sync');
 var userAns = readlineSync.question('enter your dob? ');
 var leapData=userAns%4
 if (leapData===0){console.log('your dob is leap year')}
 else(console.log('your dob is not leap year'))
+
 
